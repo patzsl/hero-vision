fd5481a6a3531568dfe844eb522bea1db761f3ec
Gabriel <147352311+patzsl@users.noreply.github.com>
2024-09-05 00:45:03 -0300

    feat: add husky eslit and prettier setup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
     preset: 'jest-preset-angular',
     setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-    globalSetup: 'jest-preset-angular/global-setup',
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
+    },
 };
